ff5c937bad4c123b7f02f58dd32316a715d869a0
Harendra231098 <harendraporipireddy98@gmail.com>
2024-05-26 15:29:50 +0530

    foodapp beginning

--- foodcrav/App.js
+++ foodcrav/App.js
@@ -1,2 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import img from './src/logo.jpg'
+/** 
+ * 
+ * Header
+ *  -logo 
+ *  -nav items
+ * Body
+ *  -Search
+ *  -RestaurantContainer
+ *    -RestaurantCard
+ * Footer
+ *  -Copyright
+ *  -links
+ *  -Address
+ *  -Contact
+ * 
+ * 
+ */
+const Header = () =>(
+  <div className="header">
+    <div className="logo-container">
+      <img className="logo" src={img} alt="food"/>
+    </div>
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+
+  </div>
+);
+
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
