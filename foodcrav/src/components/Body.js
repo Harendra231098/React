8b85c447550e8078f0bd39367eb826b26f75b3cd
Harendra231098 <harendraporipireddy98@gmail.com>
2024-05-30 04:41:23 +0530

    higher order components,promoted

--- foodcrav/src/components/Body.js
+++ foodcrav/src/components/Body.js
@@ -1,4 +1,4 @@
-import RestuarantCard from "./RestuarantCard";
+import RestuarantCard,{withPromotedLable} from "./RestuarantCard";
 import resObjs from "../utils/mockData";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
@@ -15,7 +15,7 @@ const Body = () => {
     const [listRes,setListRes] = useState([]);
     const [searchText,setSearchText]=useState("");
     const [filteredlist,setFilteredList]=useState([]);
-
+    const RestuarantPromoted = withPromotedLable(RestuarantCard);
  
 
     useEffect(()=>{
@@ -30,8 +30,8 @@ const Body = () => {
     // console.log(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     //optional chaining
-     setListRes(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-     setFilteredList(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+     setListRes(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+     setFilteredList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     }
     
@@ -46,7 +46,7 @@ if(onlineStatus === false) return <h1>Looks like you were offline</h1>
     <div className='body'>
       <div className='res-card flex justify-between'>
         <div className="search">
-            <input type='text' className="m-2.5 hover:border-collapse outline-none  border border-solid  border-b-orange-400 shadow-lg pb-1" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+            <input type='text' className="m-2.5 hover:border-collapse outline-none  border border-solid  border-b-orange-400 shadow-lg pb-1 pl-1 rounded-md" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
             <button onClick={()=>{
                 const filteredlist = listRes.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
                 setFilteredList(filteredlist);
@@ -62,7 +62,15 @@ if(onlineStatus === false) return <h1>Looks like you were offline</h1>
       </div>
       <div className='flex flex-wrap'>
         {
-          filteredlist.map((resObj)=><Link className='no-underline' key={resObj.info.id} to={"/restaurant/"+resObj.info.id}> <RestuarantCard key={resObj.info.id} resdata={resObj} /> </Link>)
+          filteredlist.map((resObj)=>
+
+          <Link className='no-underline' key={resObj.info.id} to={"/restaurant/"+resObj.info.id}> 
+
+           {/*If a restuarant is promoted add a promoted label to it*/}
+           {resObj.info.avgRating > 4 ? <RestuarantPromoted key={resObj.info.id} resdata={resObj} /> :<RestuarantCard key={resObj.info.id} resdata={resObj}/>  } 
+           
+          </Link>)
+
         }  
       </div>
     </div>
--- foodcrav/src/components/RestuarantCard.js
+++ foodcrav/src/components/RestuarantCard.js
@@ -1,11 +1,12 @@
 import { CDN_URL } from "../utils/constants";
 
-const RestuarantCard=({resdata})=>{
+const RestuarantCard=({resdata,hm})=>{
 
     const {cloudinaryImageId,name,cuisines,avgRating} = resdata?.info;
     const {deliveryTime} = resdata.info?.sla;
   return(
-    <div className='transition ease-in-out hover:scale-110 w-52 h-[22rem] p-2.5 m-2 rounded-xl bg-gray-100 border-2 hover:border-orange-200 shadow-lg'>
+    <div className='transition ease-in-out hover:scale-110 w-52 h-[22rem] p-2.5 m-2 rounded-xl bg-gray-100 border-2  shadow-lg'>
+      {hm && hm}
       <img className='rounded-xl h-2/5 w-full object-cover' 
         src={CDN_URL+cloudinaryImageId}
         alt='res-logo'
@@ -17,5 +18,19 @@ const RestuarantCard=({resdata})=>{
     </div>
   );
   }
+
+  //Higher Order Component
+ //input-Restuarant=>Restuarant Promoted
+
+ export const withPromotedLable = (RestuarantCard)=>{
+   return (props)=>{
+      return(
+        <div>
+          <label className="absolute bg-black opacity-70 text-white ml-5 mt-3 rounded-md p-1 ">Cheff Kiss</label>
+          <RestuarantCard {...props} hm={<label className="absolute bg-black opacity-70 text-white rounded-md p-1 ">Cheff Kiss</label>} />
+        </div>
+      );
+   }
+ }
 
   export default RestuarantCard;
